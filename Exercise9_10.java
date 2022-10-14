a749a0783bb5616a94a73e81872438c9447d634b
VanshikaDiwan <diwan.vanshika.23@gmail.com>
2022-10-14 16:28:11 +0530

    java question 9_10

---
+++ Exercise9_10.java
@@ -0,0 +1,10 @@
+
+public class Exercise9_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+		System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+	}
+
+}
