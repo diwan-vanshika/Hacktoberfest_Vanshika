19097eb2b10f99e4efbf5831c021002c6f651c86
VanshikaDiwan <diwan.vanshika.23@gmail.com>
2022-10-15 16:10:20 +0530

    question 21

---
+++ Exercise21.java
@@ -0,0 +1,18 @@
+
+public class Exercise21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=14;
+		String oct=Integer.toOctalString(a);
+		System.out.println(oct);
+		String b="121";
+		int b1=Integer.parseInt(b,8);
+		System.out.println(b1);
+		
+				
+
+
+	}
+
+}
