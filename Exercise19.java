27e2e14286a1f15d7e67a55946fd437d3a6081bc
VanshikaDiwan <diwan.vanshika.23@gmail.com>
2022-10-15 16:04:19 +0530

    question 19

---
+++ Exercise19.java
@@ -0,0 +1,14 @@
+
+public class Exercise19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=56;
+String bin=Integer.toBinaryString(a);
+System.out.println(bin);
+String b="100100";
+int dec=Integer.parseInt(b,2);
+System.out.println(dec);
+	}
+
+}
