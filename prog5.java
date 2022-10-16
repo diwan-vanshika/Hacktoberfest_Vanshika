35b3938ea0f842daa1d3cb9410e09ccca28c1bc2
VanshikaDiwan <diwan.vanshika.23@gmail.com>
2022-10-16 18:35:42 +0530

    prog 5

---
+++ prog5.java
@@ -0,0 +1,14 @@
+interface D
+{
+	int a=10;
+	
+}
+public class prog5 implements D{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(a);
+
+	}
+
+}
